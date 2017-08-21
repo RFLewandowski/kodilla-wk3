ce945a306da86461c5f38cf45384ad5eef43d9b6
RFLewandowski <R.F.Lewandowski@gmail.com>
2017-08-21 19:06:19 +0200

    Services added

--- src/main/java/gov/newworldorder/BookController.java
+++ src/main/java/gov/newworldorder/BookController.java
@@ -12,32 +12,32 @@ import java.util.Collection;
 public class BookController {
 
     @Autowired
-    private FakeBookDB fakeBookDB;
+    private BookService bookService;
 
     @RequestMapping(method = RequestMethod.GET)
     public Collection<Book> getAllBooks() {
-        return this.fakeBookDB.getAllBooks();
+        return this.bookService.getAllBooks();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     //TODO podpowiadanie parametrów adnotacji w Intelij nie jest zbut pomocne - w Eclipse chyba działało to lepiej
     public Book getBookById(@PathVariable("id") int id) {
-        return this.fakeBookDB.getBookById(id);
+        return this.bookService.getBookById(id);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public Book removeBookById(@PathVariable("id") int id) {
-        return this.fakeBookDB.removeBookById(id);
+        return this.bookService.removeBookById(id);
     }
 
     @RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.PUT)
     public Book updateBook(@RequestBody Book book) {
-        return fakeBookDB.updateBook(book);
+        return bookService.updateBook(book);
     }
 
     @RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
     public Book insertBook(@RequestBody Book book) {
-        return fakeBookDB.insertBook(book);
+        return bookService.insertBook(book);
     }
 
 }
---
+++ src/main/java/gov/newworldorder/BookService.java
@@ -0,0 +1,37 @@
+package gov.newworldorder;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class BookService {
+    @Autowired
+
+    private FakeBookDB FakeBookDB;
+
+    public Collection<Book> getAllBooks(){
+        return this.FakeBookDB.getAllBooks();
+    }
+    public Book getBookById(int id) {
+        return this.FakeBookDB.getBookById(id);
+    }
+
+    public Book removeBookById(int id) {
+        return this.FakeBookDB.removeBookById(id);
+    }
+
+    public Book updateBook(Book Book) {
+       return this.FakeBookDB.updateBook(Book);
+    }
+
+    public Book insertBook(Book Book) {
+        return this.FakeBookDB.insertBook(Book);
+    }
+
+
+
+
+}
